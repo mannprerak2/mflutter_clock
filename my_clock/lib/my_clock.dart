186746dc002dd11a2ba1493218136bef99f41926
Prerak Mann <mannprerak2@gmail.com>
2019-12-31 15:28:11 +0530

    Created very basic clock

---
+++ my_clock/lib/my_clock.dart
@@ -0,0 +1,118 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_clock_helper/model.dart';
+import 'package:intl/intl.dart';
+
+enum _Element {
+  background,
+  text,
+  shadow,
+}
+
+final _lightTheme = {
+  _Element.background: Color(0xFF81B3FE),
+  _Element.text: Colors.white,
+  _Element.shadow: Colors.black,
+};
+
+final _darkTheme = {
+  _Element.background: Colors.black,
+  _Element.text: Colors.white,
+  _Element.shadow: Color(0xFF174EA6),
+};
+
+class MyClock extends StatefulWidget {
+  final ClockModel model;
+
+  const MyClock(
+    this.model, {
+    Key key,
+  }) : super(key: key);
+  @override
+  _MyClockState createState() => _MyClockState();
+}
+
+class _MyClockState extends State<MyClock> {
+  DateTime _dateTime = DateTime.now();
+  Timer _timer;
+
+  @override
+  void initState() {
+    super.initState();
+    widget.model.addListener(_updateModel);
+    _updateTime();
+    _updateModel();
+  }
+
+  @override
+  void didUpdateWidget(MyClock oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (widget.model != oldWidget.model) {
+      oldWidget.model.removeListener(_updateModel);
+      widget.model.addListener(_updateModel);
+    }
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    widget.model.removeListener(_updateModel);
+    widget.model.dispose();
+    super.dispose();
+  }
+
+  void _updateModel() {
+    setState(() {});
+  }
+
+  void _updateTime() {
+    setState(() {
+      _dateTime = DateTime.now();
+      // Update once per minute
+      _timer = Timer(
+        Duration(minutes: 1) -
+            Duration(seconds: _dateTime.second) -
+            Duration(milliseconds: _dateTime.millisecond),
+        _updateTime,
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).brightness == Brightness.light
+        ? _lightTheme
+        : _darkTheme;
+    final hour =
+        DateFormat(widget.model.is24HourFormat ? 'HH' : 'hh').format(_dateTime);
+    final minute = DateFormat('mm').format(_dateTime);
+    final fontSize = MediaQuery.of(context).size.width / 3.5;
+    final offset = -fontSize / 7;
+    final defaultStyle = TextStyle(
+      color: colors[_Element.text],
+      fontSize: fontSize,
+      shadows: [
+        Shadow(
+          blurRadius: 0,
+          color: colors[_Element.shadow],
+          offset: Offset(10, 0),
+        ),
+      ],
+    );
+
+    return Container(
+      child: Center(
+        child: DefaultTextStyle(
+          style: defaultStyle,
+          child: Stack(
+            children: <Widget>[
+              Positioned(left: offset, top: 0, child: Text(hour)),
+              Positioned(right: offset, bottom: offset, child: Text(minute)),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
